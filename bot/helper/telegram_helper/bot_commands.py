1639b802253c15b7c1c609ed66054d204733fe93
deaduserbot <70665915+deaduserbot@users.noreply.github.com>
2020-10-31 00:16:08 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,9 +1,9 @@
 class _BotCommands:
     def __init__(self):
-        self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
-        self.UnzipMirrorCommand = 'unzipmirror'
-        self.TarMirrorCommand = 'tarmirror'
+        self.StartCommand = 'startos'
+        self.MirrorCommand = 'mirroros'
+        self.UnzipMirrorCommand = 'unzipmirroros'
+        self.TarMirrorCommand = 'tarmirroros'
         self.CancelMirror = 'cancel'
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
